5df78f6a206b0fc322f261a5eec71f94fa7b926d
uforya <s_oc@outlook.com>
2020-03-30 01:05:54 -0400

    Added Windows 10 support via Powershell and Task Scheduler

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# mutespotifyads
+There is a Linux version and a Windows version of this Spotify ad detection and system muting program.
+
+
+# Linux Version of mutespotifyads
 This bash function will automatically mute your system everytime a spotify ad is detected.
 
 It uses the window manager's knowledge that an "Advertisement" or a "Sponsored Message" is playing in order to mute the system while either of those strings are present in the output of "wmctrl -l". These functions are to be pasted into ~/.bashrc or ~/.profile so they can be called from the command line. The first function "mutespotifyads()" will run in a while loop in the terminal and check once a second for these string outputs. The second function, "adfree()" will call this first function within a background tmux session called "adfree".
 
 This works well in Ubuntu Studio 19.10.
+
+
+
+# Windows Version of mutespotifyads
+
+The PowerShell script "winmutespotifyads.ps1" checks for Spotify ads every 1 second within a while loop.
+
+Register "winmutespotifyads.ps1" PowerShell script as a Windows 10 task by using "Import Task" and selecting the winadfree.xml file in Task Scheduler. It is triggered to start at logon or workstation unlock. When you are importing, be sure to change the file path to the correct location of the mutespotifyads git repo. To do so: navigate to the Actions tab, double-click under the Details field, and change the path to your correct path in the Add Arguments box.
+
+In order to start and stop this task manually, open Powershell.exe with Win+X, Win+I or Win+"powershell"+Enter. To start it type "Start-ScheduledTask adfree"; to stop it type "Stop-Scheduled Task adfree".
---
+++ winmutespotifyads.ps1
@@ -0,0 +1,62 @@
+# run loop to check every 1 second if Spotify Ads are present, and if the system should be muted
+while ($true)
+{
+# this section checks to see if the [Audio] type is defined, if not, it gets defined here
+if (-not [System.Management.Automation.PSTypeName]'Audio'.Type)
+{
+Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume {
+  // f(), g(), ... are unused COM method slots. Define these if you care
+  int f(); int g(); int h(); int i();
+  int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+  int j();
+  int GetMasterVolumeLevelScalar(out float pfLevel);
+  int k(); int l(); int m(); int n();
+  int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+  int GetMute(out bool pbMute);
+}
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice {
+  int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator {
+  int f(); // Unused
+  int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+
+public class Audio {
+  static IAudioEndpointVolume Vol() {
+    var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+    IMMDevice dev = null;
+    Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+    IAudioEndpointVolume epv = null;
+    var epvid = typeof(IAudioEndpointVolume).GUID;
+    Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+    return epv;
+  }
+  public static float Volume {
+    get {float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v;}
+    set {Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty));}
+  }
+  public static bool Mute {
+    get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+    set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+  }
+}
+'@
+}
+
+
+# This section uses the above defined Type [Audio] to mute or unmute the system if Spotify Advertisement is detected
+if ($(Get-Process | Where-Object {$_.mainWindowTitle} | Format-Table mainWindowtitle -AutoSize | grep -m 1 'Advertisement\|Spotify') -And (-Not([Audio]::mute))) {[Audio]::mute = $true}
+elseif (-Not($(Get-Process | Where-Object {$_.mainWindowTitle} | Format-Table mainWindowtitle -AutoSize | grep -m 1 'Advertisement\|Spotify')) -And ([Audio]::mute)) {[Audio]::mute = $false}
+
+
+# pause this program for 1 second, then check for Spotify ads again
+Start-Sleep -s 1
+}
